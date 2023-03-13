433817e2ee278131606db65fa5d837e6f1f8013c
saladd <spad1239@gmail.com>
2023-03-13 16:08:39 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OOP_LAB_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox_CTRL = new System.Windows.Forms.CheckBox();
+            this.checkBox_Multiple = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // checkBox_CTRL
+            // 
+            this.checkBox_CTRL.AutoCheck = false;
+            this.checkBox_CTRL.AutoSize = true;
+            this.checkBox_CTRL.Location = new System.Drawing.Point(14, 16);
+            this.checkBox_CTRL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox_CTRL.Name = "checkBox_CTRL";
+            this.checkBox_CTRL.Size = new System.Drawing.Size(54, 24);
+            this.checkBox_CTRL.TabIndex = 1;
+            this.checkBox_CTRL.Text = "Ctrl";
+            this.checkBox_CTRL.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Multiple
+            // 
+            this.checkBox_Multiple.AutoSize = true;
+            this.checkBox_Multiple.Location = new System.Drawing.Point(14, 49);
+            this.checkBox_Multiple.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox_Multiple.Name = "checkBox_Multiple";
+            this.checkBox_Multiple.Size = new System.Drawing.Size(72, 24);
+            this.checkBox_Multiple.TabIndex = 2;
+            this.checkBox_Multiple.Text = "Single";
+            this.checkBox_Multiple.UseVisualStyleBackColor = true;
+            this.checkBox_Multiple.CheckedChanged += new System.EventHandler(this.checkBox_Multiple_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(14, 83);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1234, 574);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.checkBox_Multiple);
+            this.Controls.Add(this.checkBox_CTRL);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CheckBox checkBox_CTRL;
+        private CheckBox checkBox_Multiple;
+        private Panel panel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+using System.Windows.Forms;
+
+namespace OOP_LAB_4
+{
+    public partial class Form1 : Form
+    {
+        List<Circle> circles;
+        CheckBoxs model;
+        
+        int radius;
+
+        int CursorX;
+        int CursorY;
+
+        Graphics g;
+        Pen RedPen;
+        Pen BlackPen;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.MouseUp += Form1_MouseUp;
+
+            model = new CheckBoxs(checkBox_CTRL.Checked, checkBox_Multiple.Checked);
+            model.observers += new EventHandler(UpdateFromCheckBoxs);
+
+            circles = new();
+
+            radius = 50;
+
+            CursorX = 0;
+            CursorY = 0;
+
+            g = panel1.CreateGraphics();
+            RedPen = new Pen(Color.Red, 1);
+            BlackPen = new Pen(Color.Black, 1);
+        }
+        public void UpdateFromCheckBoxs(object sender, EventArgs e)
+        {
+            checkBox_CTRL.Checked = model.get_check_ctrl();
+            checkBox_Multiple.Checked = model.get_check_multiple();
+        }
+       
+        private void checkBox_Multiple_CheckedChanged(object sender, EventArgs e)
+        {
+            model.setMultiple(checkBox_Multiple.Checked);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Circle c in circles)
+            {
+                if (c.getColor())
+                    c.draw(g, RedPen);
+                else
+                    c.draw(g, BlackPen);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            bool check = true;
+
+            CursorX = e.X;
+            CursorY = e.Y;
+
+            foreach (Circle c in circles)
+            {
+                if (c.inCircle(CursorX, CursorY))
+                {
+                    model.setCtrl(false);
+                    check = false;
+                    c.color_changed();
+                    if (model.get_check_multiple())
+                    {
+                        break;
+                    }
+                }
+                else if (Form.ModifierKeys != Keys.Control)
+                {
+                    c.setColor(false);
+                    model.setCtrl(false);
+                }
+                else
+                {
+                    model.setCtrl(true);
+                }
+            }
+
+            if (check)
+            {
+                foreach (Circle c in circles)
+                {
+                    c.setColor(false);
+                }
+                Circle circle = new Circle(CursorX, CursorY, radius);
+                circles.Add(circle);
+                model.setCtrl(false);
+            }
+            panel1.Invalidate();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                g.Clear(SystemColors.Control);
+                circles.Clear();
+            }
+        }
+    }
+
+    class CheckBoxs
+    {
+        private bool check_ctrl;
+        private bool check_multiple;
+        public EventHandler observers;
+        public CheckBoxs()
+        {
+            check_ctrl = false;
+            check_multiple = false;
+        }
+        public CheckBoxs(bool ctrl, bool multiple)
+        {
+            check_ctrl = ctrl;
+            check_multiple = multiple;
+        }
+        public bool get_check_ctrl()
+        {
+            return check_ctrl;
+        }
+        public bool get_check_multiple()
+        {
+            return check_multiple;
+        }
+
+        public void setCtrl(bool check)
+        {
+            check_ctrl = check;
+            observers.Invoke(this, null);
+        }
+        public void setMultiple(bool multiple)
+        {
+            check_multiple = multiple;
+            observers.Invoke(this, null);
+        }
+
+    }
+    class Circle
+    {
+        private int x, y, rad;
+        private bool color; // true => red false => black
+        public Circle(int x, int y, int rad) {
+            this.x = x;
+            this.y = y;
+            this.rad = rad;
+            color = true;
+        }
+        
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public int getRad() 
+        { 
+            return rad;
+        }
+        public int getSquareRad()
+        {
+            return rad*rad;
+        }
+        public bool getColor()
+        {
+            return color;
+        }
+        public void setColor(bool color)
+        {
+            this.color = color;
+        }
+        public void color_changed()
+        {
+            color = !color;
+        }
+        public int getSquareDistToPointFromCentre(int point_x, int point_y)
+        {
+            int squareDist = (point_x - x) * (point_x - x) + (point_y - y) * (point_y - y);
+            return squareDist;
+        }
+
+        public void draw(Graphics g, Pen pen)
+        {
+            g.DrawEllipse(pen, x - rad, y - rad, rad * 2, rad * 2);
+        }
+        public bool inCircle(int point_x, int point_y) {
+            if(getSquareDistToPointFromCentre(point_x, point_y) < getSquareRad())
+            return true;
+            return false;
+        }
+
+
+    }
+}
